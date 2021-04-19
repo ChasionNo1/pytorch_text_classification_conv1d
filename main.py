42aa5af3e5b8aac7869e7d3d8688277ae509f4c3
lucaslingle <lucasdaxlingle@gmail.com>
2021-04-19 01:27:20 -0700

    commit

--- main.py
+++ main.py
@@ -1,47 +1,14 @@
 import torch as tc
-import torchtext as tt
 from functools import partial
 from datasets import IMDBDatasetEmbedded
-#from utils import get_tokenizer, get_vocab, text_pipeline, label_pipeline
 from classifier import Conv1dTextClassifier
 from runner import Runner
 
-"""
-# Download datasets. These text datasets are IterableDatasets.
-training_data = tc.utils.data.BufferedShuffleDataset(tt.datasets.IMDB(split='train'), buffer_size=25000)
-test_data = tc.utils.data.BufferedShuffleDataset(tt.datasets.IMDB(split='test'), buffer_size=25000)
-"""
-
+# Datasets.
 training_data = IMDBDatasetEmbedded(root='data', train=True)
 test_data = IMDBDatasetEmbedded(root='data', train=False)
 
-"""
-# Create a map to be applied to each batch item.
-tokenizer = get_tokenizer()
-vocab = get_vocab(training_data, tokenizer)
-text_preprocessing = partial(text_pipeline, tokenizer=tokenizer, vocab=vocab)
-def collate_batch(batch):
-    x_list = []
-    y_list = []
-    for y, x in batch:
-        x_list.append(text_preprocessing(x))
-        y_list.append(label_pipeline(y))
-    X = tc.tensor(x_list, dtype=tc.float32)
-    Y = tc.tensor(y_list, dtype=tc.int64)
-    return X, Y
-"""
-
-tokenizer = training_data.tokenizer
-vocab = training_data.vocab
-text_preprocessing = partial(training_data.text_pipeline, tokenizer=tokenizer, vocab=vocab)
-
-"""
-# Create data loaders to batch, shuffle, and map the data.
-batch_size = 50
-train_dataloader = tc.utils.data.DataLoader(training_data, batch_size=batch_size, collate_fn=collate_batch)
-test_dataloader = tc.utils.data.DataLoader(test_data, batch_size=batch_size, collate_fn=collate_batch)
-"""
-
+# Dataloaders.
 batch_size = 50
 train_dataloader = tc.utils.data.DataLoader(training_data, batch_size=batch_size)
 test_dataloader = tc.utils.data.DataLoader(test_data, batch_size=batch_size)
@@ -64,14 +31,19 @@ try:
 except Exception:
     print('no checkpoint found. training from scratch...')
 
-# Instantiate a runner to run the training epochs and monitor performance.
+# Runner.
 runner = Runner(max_epochs=1, verbose=True)
 runner.run(model, train_dataloader, test_dataloader, device, criterion, optimizer)
 print("All done!")
 
-# Run a demo
+# Demo.
 model.eval()
 hypothetical_review = "The movie was good, but there were some over the top moments especially in the action scenes. Overall B+."
+
+tokenizer = training_data.tokenizer
+vocab = training_data.vocab
+text_preprocessing = partial(training_data.text_pipeline, tokenizer=tokenizer, vocab=vocab)
+
 x = tc.from_numpy(text_preprocessing(hypothetical_review))
 y_logits = model.forward(tc.unsqueeze(x, dim=0))
 y_probs = tc.nn.Softmax(dim=-1)(y_logits)
