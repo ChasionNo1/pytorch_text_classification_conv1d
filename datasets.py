3f9ff347c8184fa4c8459145106f5bc1907d6484
lucaslingle <lucasdaxlingle@gmail.com>
2021-04-19 01:22:20 -0700

    commit

---
+++ datasets.py
@@ -0,0 +1,54 @@
+import torch as tc
+import torchtext as tt
+from collections import Counter
+import numpy as np
+
+
+class IMDBDatasetEmbedded(tc.utils.data.IterableDataset):
+    # This is cleaner than using collate_fn is the dataloader.
+    # Investigation shows it is faster, as well.
+    def __init__(self, root, train):
+        self.root = root
+        self.tokenizer = tt.data.utils.get_tokenizer('basic_english')
+        self.vocab = self.get_vocab(self.root, self.tokenizer)
+        self.dataset = tc.utils.data.BufferedShuffleDataset(
+            tt.datasets.IMDB(split='train' if train else 'test'),
+            buffer_size=50000
+        )
+        self.n_tokens = 400
+
+    @staticmethod
+    def get_vocab(root, tokenizer):
+        train_iter = tt.datasets.IMDB(root=root, split='train')
+
+        counter = Counter()
+        for y, X in train_iter:
+            counter.update(tokenizer(X))
+
+        vectors = tt.vocab.FastText(language='en')
+        vocab = tt.vocab.Vocab(counter, vectors=vectors)
+        return vocab
+
+    def text_pipeline(self, text, tokenizer, vocab):
+        vectors = np.stack([vocab.vectors[vocab.stoi[token]] for token in tokenizer(text)], axis=0)
+        vectors = vectors[0:self.n_tokens]
+        pad_len = self.n_tokens-len(vectors)
+        pad_shape = (pad_len, 300)
+        if pad_len > 0:
+            vectors = np.concatenate([vectors, np.zeros(dtype=np.float32, shape=pad_shape)], axis=0)
+        vectors = np.transpose(vectors) # convert to NCH format.
+        return vectors
+
+    def label_pipeline(self, y):
+        d = {
+            "neg": 0,
+            "pos": 1
+        }
+        return d[y]
+
+    def __iter__(self):
+        fn = lambda X, y: (
+            self.text_pipeline(X, self.tokenizer, self.vocab),
+            self.label_pipeline(y)
+        )
+        return (fn(X,y) for y, X in self.dataset.__iter__())
--- main.py
+++ main.py
@@ -1,25 +1,50 @@
 import torch as tc
 import torchtext as tt
 from functools import partial
-from utils import get_tokenizer, get_vocab, text_pipeline, label_pipeline
+from datasets import IMDBDatasetEmbedded
+#from utils import get_tokenizer, get_vocab, text_pipeline, label_pipeline
 from classifier import Conv1dTextClassifier
 from runner import Runner
 
-
+"""
 # Download datasets. These text datasets are IterableDatasets.
-training_data = tt.datasets.IMDB(split='train')
-test_data = tt.datasets.IMDB(split='test')
+training_data = tc.utils.data.BufferedShuffleDataset(tt.datasets.IMDB(split='train'), buffer_size=25000)
+test_data = tc.utils.data.BufferedShuffleDataset(tt.datasets.IMDB(split='test'), buffer_size=25000)
+"""
 
+training_data = IMDBDatasetEmbedded(root='data', train=True)
+test_data = IMDBDatasetEmbedded(root='data', train=False)
+
+"""
 # Create a map to be applied to each batch item.
 tokenizer = get_tokenizer()
 vocab = get_vocab(training_data, tokenizer)
 text_preprocessing = partial(text_pipeline, tokenizer=tokenizer, vocab=vocab)
-collate_fn = lambda y,x: (text_preprocessing(x), label_pipeline(y))
+def collate_batch(batch):
+    x_list = []
+    y_list = []
+    for y, x in batch:
+        x_list.append(text_preprocessing(x))
+        y_list.append(label_pipeline(y))
+    X = tc.tensor(x_list, dtype=tc.float32)
+    Y = tc.tensor(y_list, dtype=tc.int64)
+    return X, Y
+"""
 
+tokenizer = training_data.tokenizer
+vocab = training_data.vocab
+text_preprocessing = partial(training_data.text_pipeline, tokenizer=tokenizer, vocab=vocab)
+
+"""
 # Create data loaders to batch, shuffle, and map the data.
 batch_size = 50
-train_dataloader = tc.utils.data.DataLoader(training_data, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
-test_dataloader = tc.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
+train_dataloader = tc.utils.data.DataLoader(training_data, batch_size=batch_size, collate_fn=collate_batch)
+test_dataloader = tc.utils.data.DataLoader(test_data, batch_size=batch_size, collate_fn=collate_batch)
+"""
+
+batch_size = 50
+train_dataloader = tc.utils.data.DataLoader(training_data, batch_size=batch_size)
+test_dataloader = tc.utils.data.DataLoader(test_data, batch_size=batch_size)
 
 # Device.
 device = "cuda" if tc.cuda.is_available() else "cpu"
@@ -47,8 +72,8 @@ print("All done!")
 # Run a demo
 model.eval()
 hypothetical_review = "The movie was good, but there were some over the top moments especially in the action scenes. Overall B+."
-x = text_preprocessing(hypothetical_review)
-y_logits = model.forward(x)
+x = tc.from_numpy(text_preprocessing(hypothetical_review))
+y_logits = model.forward(tc.unsqueeze(x, dim=0))
 y_probs = tc.nn.Softmax(dim=-1)(y_logits)
 print(hypothetical_review)
 print(y_probs)
